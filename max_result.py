f16b1841d60e5d1752dffe79b988b8c3f52b7c3f
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-07-26 14:41:52 +0300

    Renamed.

---
+++ max_result.py
@@ -0,0 +1,17 @@
+n = int(input())
+nums = sorted([int(input()) for _ in range(n)])
+result = 0
+
+if n > 2:
+    for i in range(n-1):
+        sum_of_i = nums[i] + nums[i + 1]
+        for j in range(i+1, n):
+            if nums[j] < sum_of_i:
+                var = sum(nums[i:j+1])
+                result = max(result, var)
+            else:
+                break
+else:
+    result = sum(nums)
+
+print(result)                
