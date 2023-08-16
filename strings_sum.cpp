96643586eb8d55041c50203232b18cdb33be43ff
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-08-16 21:33:38 +0300

    Ref rabbit.cpp and created algorithm strings_sum.cpp.

--- strings_sum.cpp
+++ strings_sum.cpp
@@ -12,14 +12,14 @@ string strings_sum(string str1, string str2) {
   if (str1_size > str2_size) {
     for (int i = 0; i < (str1_size - str2_size); ++i) {
       str2.insert(0, "0");
+      ++str2_size;
     }
   } else {
     for (int i = 0; i < (str2_size - str1_size); ++i) {
       str1.insert(0, "0");
+      ++str1_size;
     }
   }
-  str1_size = str1.size();
-  str2_size = str2.size();
   for (int i = str1_size - 1; i >= 0; --i) {
     int var = (int)(str1[i] - '0') + (int)(str2[i] - '0') + p;
     if (var > 9) {
@@ -36,25 +36,7 @@ string strings_sum(string str1, string str2) {
 }
 
 int main() {
-  int k, n;
-  cin >> k >> n;
-  if (k == 1) {
-    cout << 1;
-  } else {
-    int size_A = n + k;
-    vector<string> A(size_A, "0");
-    A[k - 1] = "1";
-    A[k] = "1";
-    A[k + 1] = "2";
-    for (int i = k + 2; i < size_A; ++i) {
-      for (int j = 1; j <= k; ++j) {
-        A[i] = strings_sum(A[i], A[i - j]);
-      }
-    }
-    //   for (string s : A) {
-    //     cout << s << "\t";
-    //   }
-    cout << A[size_A - 1];
-  }
-  return 0;
+  string s, s2;
+  cin >> s >> s2;
+  cout << strings_sum(s, s2);
 }
