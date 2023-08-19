5a6a02eb01f79b92f552926299813eda80ed5713
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-08-19 17:24:10 +0300

    Save.

--- nails.cpp
+++ nails.cpp
@@ -17,21 +17,31 @@ int main() {
   }
   sort(A.begin(), A.end());
 
-  result += A[1] - A[0];
-  m[A[1]] = true;
+  if (n == 2) {
+    result += A[1] - A[0];
+  } else if (n == 3) {
+    result += (A[1] - A[0]) + (A[2] - A[1]);
+  } else {
+    result += A[1] - A[0];
+    // cout << "first result: " << result << endl;
+    result += A[n - 1] - A[n - 2];
+    m[A[n - 2]] = true;
+    // cout << "second result: " << result << endl;
 
-  for (size_t p = 2; p < n; ++p) {
-    if (!m.count(A[p])) {
-      if (p == n - 1) {
-        result += A[p] - A[p - 1];
-      } else {
+    for (size_t p = 2; p < n - 2; ++p) {
+      // cout << "p = " << p << endl;
+      if (!m.count(A[p])) {
         int var1 = A[p + 1] - A[p];
+        // cout << "var1 = " << var1 << endl;
         int var2 = A[p] - A[p - 1];
-        if (var1 <= var2 && p + 1 != n - 2) {
+        // cout << "var2 = " << var2 << endl;
+        if (var1 <= A[p + 2] - A[p + 1]) {
           result += var1;
           m[A[p + 1]] = true;
+          // cout << "var1 <= var2" << endl;
         } else {
           result += var2;
+          // cout << "var1 > var2" << endl;
         }
       }
     }
