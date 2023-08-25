4f9c0d1ef6b0763f80cf574d4d2c3779095f17fe
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-08-25 17:58:20 +0300

    university_life.cpp but not working.

--- Yandex_competition/univercity_life.cpp
+++ Yandex_competition/univercity_life.cpp
@@ -6,19 +6,22 @@
 using namespace std;
 
 int main() {
-  int n, var1, var2;
+  int n;
   cin >> n;
   vector<int> X(n);
-  map<int, int> m;
+  map<int, int> S;
+
   for (int i = 0; i < n; ++i) {
-    cin >> var1 >> var2;
-    X[i] = var1;
-    m[X[i]] = var2;
+    cin >> X[i];
+    cin >> S[X[i]];
   }
 
   sort(X.begin(), X.end());
+  vector<int> m{0, S[X[0]]};
 
-  for (int x : X) {
+  for (int i = 2; i <= n; ++i) {
+    m.push_back(min(m[i - 2], m[i - 1]) + S[X[i - 1]]);
   }
+  cout << m[n];
   return 0;
 }
