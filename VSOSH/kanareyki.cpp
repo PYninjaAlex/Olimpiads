3d80049e367a18a1e6b43121cc0af3ea9f480783
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-09-20 16:51:43 +0300

    kanareyki.cpp

--- VSOSH/kanareyki.cpp
+++ VSOSH/kanareyki.cpp
@@ -12,9 +12,11 @@ int main() {
     cin >> A[i];
   }
   sort(A.begin(), A.end());
-  if (A[n - 1] < A[n - 2] + A[n - 3]) {
-    cout << A[n - 1] + A[n - 2] + A[n - 3];
-    return 0;
+  for (int i = n - 1; i > 1; --i) {
+    if (A[i] < A[i - 1] + A[i - 2]) {
+      cout << A[i] + A[i - 1] + A[i - 2];
+      return 0;
+    }
   }
   cout << 0;
   return 0;
